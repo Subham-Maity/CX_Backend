3db958565ede3d440b8f3bae5d30f1897b63f3b5
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-01-10 22:59:46 +0530

    Repository -> User Repo.java
     Interface UserRepo extends JpaRepository <user, Integer>

---
+++ backend/src/main/java/com/manage/backend/codexam/repository/UserRepo.java
@@ -0,0 +1,7 @@
+package com.manage.backend.codexam.repository;
+
+import com.manage.backend.codexam.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Integer> {
+}
